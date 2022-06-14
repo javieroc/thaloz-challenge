2b6afa0a811eb0ac7cc97bd159e2730a24ef08f1
Javier Ocampo <ferroxido@gmail.com>
2022-06-14 10:13:47 -0300

    fix order

--- challenge.js
+++ challenge.js
@@ -7,8 +7,33 @@ function getSquares (arr) {
   return squares
 }
 
+function getSorted (arr) {
+  let aux = null
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[i] > arr[j]) {
+        aux = arr[j]
+        arr[j] = arr[i]
+        arr[i] = aux
+      }
+    }
+  }
+  return arr
+}
+
+function getSquaresSorted (arr) {
+  const squares = getSquares(arr)
+  const sorted = getSorted(squares)
+  console.log(
+    `input array: ${JSON.stringify(arr)} / output array: ${JSON.stringify(sorted)}`
+  )
+  return sorted
+}
+
 const collectionUtils = {
-  getSquares
+  getSquares,
+  getSorted,
+  getSquaresSorted
 }
 
 module.exports = collectionUtils
--- challenge.test.js
+++ challenge.test.js
@@ -1,24 +1,24 @@
-const { getSquares } = require('./challenge')
+const { getSquaresSorted } = require('./challenge')
 
 describe('sorted squared array', () => {
   test('test case 1', () => {
     const testCase1 = { array: [1, 2, 3, 5, 6, 8, 9] }
 
     const expected = [1, 4, 9, 25, 36, 64, 81]
-    expect(getSquares(testCase1.array)).toEqual(expect.arrayContaining(expected))
+    expect(getSquaresSorted(testCase1.array)).toEqual(expected)
   })
 
   test('test case 2', () => {
     const testCase2 = { array: [-2, -1] }
 
     const expected = [1, 4]
-    expect(getSquares(testCase2.array)).toEqual(expect.arrayContaining(expected))
+    expect(getSquaresSorted(testCase2.array)).toEqual(expected)
   })
 
   test('test case 3', () => {
     const testCase3 = { array: [-10, -5, 0, 5, 10] }
 
     const expected = [0, 25, 25, 100, 100]
-    expect(getSquares(testCase3.array)).toEqual(expect.arrayContaining(expected))
+    expect(getSquaresSorted(testCase3.array)).toEqual(expected)
   })
 })
